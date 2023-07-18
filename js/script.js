8ac2886d6368f06708d63fc7b1c441c46a9aa36f
Adam Britt <adamrussellbritt@gmail.com>
2023-07-18 11:25:40 -0500

    subscription math

--- js/script.js
+++ js/script.js
@@ -7,10 +7,23 @@ var subDuration = 1;
 
 subTypeElement.addEventListener("change", function(e) {
     subType = e.target.value;
-    console.log(subType);
+    // console.log(subType);
+    updateSubscriptionDiv();
 });
 
 subDurationElement.addEventListener("change", function(e) {
     subDuration = Number(e.target.value);
-    console.log(subDuration);
+    // console.log(subDuration);
+    updateSubscriptionDiv();
 });
+
+var updateSubscriptionDiv = function() {
+    var monthlyCost = 5;
+    if(subType === "standard") {
+        monthlyCost = 7;
+    } else if (subType === "premium") {
+        monthlyCost = 10;
+    }
+    var total = subDuration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
